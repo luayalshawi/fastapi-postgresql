a1005652dac1d32fbf1e926ac4b73eb162261db2
Luay Alshawi <luay.alshawi@gmail.com>
2023-11-21 02:20:48 +0300

    initial project

---
+++ README.md
@@ -0,0 +1,36 @@
+# 🚀  fastapi-postgresql🎉
+
+Sample project for KFU-CODE. A two days training session on Sunday & Monday Nov 26-27 2023 from 3-5 pm. 
+
+
+## Setup & Run 🏃‍♂️
+
+- Download and install Docker with compose
+
+-   Setup and Build:
+
+    set up a .env file with your configuration.  For a basic version for local testing use:  
+    ```bash
+    cp dot-env-template .env
+    ```
+    Be aware that .env is *excluded from git* because it contains secrets, API keys and so on.  **Never put your .env file into git.**
+
+    Then build and start the test/debug stack with:
+    ```bash
+    docker-compose up --build
+    ```
+
+    Then:
+    - Visit http://localhost:4000/docs for the interactive API docs (Swagger). For initial super-username and password to first authenticate see your **.env** file.
+    - Modify your code, which is linked into the *fastapi-app* container and watch uvicorn auto-restart your app when changes have been made.
+    - Run pytest in your container with `docker exec fastapi-app bash ./test.sh [optional parameters]`
+    - Visit http://localhost:5050/ for the PostgreSQL administrator. Upon first use you'll need to register your DB server using your **.env** file.
+
+## Features 🌈
+
+1. **FastAPI Magic** - Because speed is not just about Fast & Furious movies!
+2. **PostgreSQL Integration** - Store data like a pro. No more data loss nightmares!
+3. **Docker Integration** - Set sail with a consistent environment. Because why let tech discrepancies ruin your day?
+
+Base strucutre is taken from https://github.com/bibektimilsina000/FastAPI-PgStarterKit/tree/main
+
---
+++ app/core/config.py
@@ -0,0 +1,91 @@
+import secrets
+from typing import Any, List, Optional, Union
+
+from dotenv import load_dotenv
+from pydantic import (AnyHttpUrl, AnyUrl, EmailStr, HttpUrl, PostgresDsn,
+                      ValidationInfo, field_validator)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = os.getenv("ACCESS_TOKEN_SECRET", "") #secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    SERVER_HOST: AnyHttpUrl
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    @classmethod
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    @classmethod
+    def assemble_db_connection(cls, v: Optional[str], info: ValidationInfo) -> Any:
+        if isinstance(v, str):
+            return v
+        user = info.data.get("POSTGRES_USER")
+        password = info.data.get("POSTGRES_PASSWORD")
+        host = info.data.get("POSTGRES_SERVER")
+        db = info.data.get("POSTGRES_DB")
+
+        if all([user, password, host, db]):
+            return f"postgresql://{user}:{password}@{host}/{db}"
+        else:
+            return None
+
+    SMTP_TLS: bool = True
+    SMTP_PORT: Optional[int] = None
+    SMTP_HOST: Optional[str] = None
+    SMTP_USER: Optional[str] = None
+    SMTP_PASSWORD: Optional[str] = None
+    EMAILS_FROM_EMAIL: Optional[EmailStr] = None
+    EMAILS_FROM_NAME: Optional[str] = None
+
+    @field_validator("EMAILS_FROM_NAME", mode="before")
+    @classmethod
+    def get_project_name(cls, v: Optional[str], info: ValidationInfo) -> str:
+        if not v:
+            return info.data.get("PROJECT_NAME")
+        return v
+
+    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
+    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
+    EMAILS_ENABLED: bool = False
+
+    @field_validator("EMAILS_ENABLED", mode="before")
+    @classmethod
+    def get_emails_enabled(cls, v: bool, info: ValidationInfo) -> bool:
+        return bool(
+            info.data.get("SMTP_HOST")
+            and info.data.get("SMTP_PORT")
+            and info.data.get("EMAILS_FROM_EMAIL")
+        )
+
+    EMAIL_TEST_USER: EmailStr = "test@example.com"
+    FIRST_SUPERUSER: EmailStr
+    FIRST_SUPERUSER_PASSWORD: str
+    USERS_OPEN_REGISTRATION: bool = False
+    
+    model_config = SettingsConfigDict(case_sensitive=True)
+
+
+settings = Settings()
